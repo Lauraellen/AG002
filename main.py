698d6e2364199da54a9b246207aa8b9cf2c9f9cb
DaianeDionisio <daianedionisio2000@gmail.com>
2023-06-15 21:36:19 -0300

    update input

--- main.py
+++ main.py
@@ -44,7 +44,96 @@ tree_predict = tree.predict(X_test)
 tree_score = accuracy_score(y_test, tree_predict)
 print(f"Pontuação Decision Tree:{tree_score}")
 
-teste = [[4, 1, 3, 4, 1, 1, 2, 2, 2]]
-print(teste)
-resposta = tree.predict(teste)
-print(resposta)
+
+print("Entre com os dados para verificar a chace de recorrência da doença após o tratamento:")
+age = input("Entre com a idade da paciente:")
+menopause = input("Com relação a menopausa da paciente, selecione uma das opções: 1 - Menopausa antes dos 40 2 - Menopausa depois dos 40 3 - Pré Menopausa")
+tumorSize = input("Indique a faixa do diâmetro do tumor em mm:")
+invNodes = input("Indique o número de linfonodos axilares:")
+nodeCaps = input("Informe se há penetração do tumor na cápsula do linfonodo:")
+degMalig = input("Informe o grau de malignidade do tumor: 1 - Menor 2 - Médio 3 - Maior")
+breast = input("Informe a mama: 1 - Esquerda 2 - Direita")
+breastQuad = input("Informe o quadrante da mama afetado: 1 - esquerda-cima 2 - esquerda-baixo 3 - direita - cima 4 - direita - baixo 5 - centro")
+irradiat = input("Há histórico de radioterapia? 1 - Não 2- Sim")
+
+ageValue = 0
+if age >= 10 and age <= 19:
+    ageValue = 1
+elif age >= 20 and age <= 29:
+    ageValue = 2
+elif age >= 30 and age <= 39:
+    ageValue = 3
+elif age >= 40 and age <= 49:
+    ageValue = 4
+elif age >= 50 and age <= 59:
+    ageValue = 5
+elif age >= 60 and age <= 69:
+    ageValue = 6
+elif age >= 70 and age <= 79:
+    ageValue = 7
+elif age >= 80 and age <= 89:
+    ageValue = 8
+elif age >= 90 and age <= 99:
+    ageValue = 9
+
+tumorSizeValue = 0
+if tumorSize <= 4:
+    tumorSizeValue = 1
+elif tumorSize >= 5 and age <= 9:
+    ageValue = 2
+elif tumorSize >= 10 and age <= 14:
+    ageValue = 3
+elif tumorSize >= 15 and age <= 19:
+    ageValue = 4
+elif tumorSize >= 20 and age <= 24:
+    ageValue = 5
+elif tumorSize >= 25 and age <= 29:
+    ageValue = 6
+elif tumorSize >= 30 and age <= 34:
+    ageValue = 7
+elif tumorSize >= 35 and age <= 39:
+    ageValue = 8
+elif tumorSize >= 40 and age <= 44:
+    ageValue = 9
+elif tumorSize >= 45 and age <= 49:
+    ageValue = 10
+elif tumorSize >= 50 and age <= 54:
+    ageValue = 11
+elif tumorSize >= 55 and age <= 59:
+    ageValue = 12
+
+invNodesValue = 0
+if invNodes <= 2:
+    invNodesValue = 1
+elif invNodes >= 3 and invNodes <= 5:
+    invNodesValue = 2
+elif invNodes >= 6 and invNodes <= 8:
+    invNodesValue = 3
+elif invNodes >= 9 and invNodes <= 11:
+    invNodesValue = 4
+elif invNodes >= 12 and invNodes <= 14:
+    invNodesValue = 5
+elif invNodes >= 15 and invNodes <= 17:
+    invNodesValue = 6
+elif invNodes >= 18 and invNodes <= 20:
+    invNodesValue = 7
+elif invNodes >= 21 and invNodes <= 23:
+    invNodesValue = 8
+elif invNodes >= 24 and invNodes <= 26:
+    invNodesValue = 9
+elif invNodes >= 17 and invNodes <= 29:
+    invNodesValue = 10
+elif invNodes >= 30 and invNodes <= 32:
+    invNodesValue = 11
+elif invNodes >= 33 and invNodes <= 35:
+    invNodesValue = 12
+elif invNodes >= 36 and invNodes <= 39:
+    invNodesValue = 13
+
+
+dados = [[ageValue, menopause, tumorSizeValue, invNodesValue, nodeCaps, degMalig, breast, breastQuad, irradiat]]
+resposta = tree.predict(dados)
+if resposta == 1:
+    print("Não é chances de recorrência da doença após o tratamento.")
+elif resposta == 2:
+    print("Há chances de recorrência da doença após o tratamento.")
